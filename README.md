95c30413a760ea007bfa3c8af8e189e652b41fe3
Jakub Kottnauer <jk@jakubkottnauer.com>
2018-05-13 22:32:36 +0200

    Initial (dirty) implementation

---
+++ README.md
@@ -0,0 +1,2 @@
+# ERS Workout Booking Bot
+This is a simple bot for booking workouts at my favorite gym. Because ain't nobody got time for doing that manually all the time.
---
+++ index.js
@@ -0,0 +1,111 @@
+import 'babel-core/register'
+import 'babel-polyfill'
+import moment from 'moment'
+import puppeteer from 'puppeteer'
+
+const URL = 'https://ersworkout.isportsystem.cz'
+
+const cookies = {
+  kuba: {
+    email: '',
+    password: '',
+    PHPSESSID: ''
+  },
+  tyna: {
+    email: '',
+    password: '',
+    PHPSESSID: ''
+  }
+}
+
+const dayTimePairs = [
+  [5, '17:00', 'Lněničková'], // Friday 5pm
+  [7, '17:00', 'Přibyl'] // Sunday 5pm
+]
+
+// Pass index of the next day to find the next instance of the day. 1 is Monday, 7 is Sunday
+const getNextDayInstance = day =>
+  moment().isoWeekday() < day
+    ? moment().isoWeekday(day)
+    : moment()
+      .add(1, 'weeks')
+      .isoWeekday(day)
+
+const WAIT_FOR = 1000
+
+const selectDateInCalendar = async (page, day, month) => {
+  const isCorrectMonth = (await page.$$(`td[data-month="${month}"]`)).length > 0
+  if (!isCorrectMonth) {
+    const nextMonth = await page.$$(`td[data-handler="next"]`)
+    await nextMonth.click()
+    await page.waitFor(WAIT_FOR)
+  }
+
+  const calendarElems = await page.$$('.ui-datepicker-calendar tbody a')
+  for (let elem of calendarElems) {
+    const text = await (await elem.getProperty('innerHTML')).jsonValue()
+
+    if (text === day.toString()) {
+      await elem.click()
+      await page.waitFor(WAIT_FOR)
+      break
+    }
+  }
+}
+
+const selectTraining = async (page, time, name) => {
+  const trainingsOnDay = await page.$$('a.slot')
+  for (let training of trainingsOnDay) {
+    const text = await (await training.getProperty('innerHTML')).jsonValue()
+
+    if (text.indexOf(`>${time}`) > 0 && text.indexOf(name) > 0) {
+      console.log(text)
+      await training.click()
+      await page.waitFor(WAIT_FOR)
+      break
+    }
+  }
+}
+
+const checkTerms = async page => {
+  const terms = await page.$('input#terms')
+  await terms.click()
+}
+
+const confirm = async page => {
+  const confirmButton = (await page.$$('.ui-dialog-buttonset button'))[1] // Cancel button, change to 0 for real confirmation!
+  //await page.screenshot({ path: Math.random() + 'yes.png' })
+  // await confirmButton.click()
+}
+
+;(async () => {
+  const browser = await puppeteer.launch()
+
+  for (let client of Object.keys(cookies)) {
+    const page = await browser.newPage()
+
+    await page.setCookie(
+      ...['PHPSESSID', 'email', 'password'].map(c => ({
+        name: c,
+        value: cookies[client][c],
+        expires: -1,
+        domain: 'ersworkout.isportsystem.cz',
+        path: '/',
+        session: true
+      }))
+    )
+
+    for (let pair of dayTimePairs) {
+      const nextDay = getNextDayInstance(pair[0])
+      await page.goto(URL)
+      await page.waitFor(WAIT_FOR)
+
+      await selectDateInCalendar(page, nextDay.date(), nextDay.month())
+      await selectTraining(page, pair[1], pair[2])
+      await checkTerms(page)
+      await confirm(page)
+    }
+  }
+
+  await browser.close()
+})()
